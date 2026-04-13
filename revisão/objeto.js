0367db6092bfd75de1b7aa508ca3ecf398fc7dcb
Aluno Técnico ADS2 <zotto.s.barbara@gmail.com>
2026-04-13 16:54:41 -0300

    coleções

---
+++ revisão/objeto.js
@@ -0,0 +1,29 @@
+const pessoa = {
+    nome: "João",
+    sobrenome: "Roccella",
+    idade: 40,
+    cargo: "Instrutor",
+};
+const entidade = new Map([
+    ["nome", "João"],
+    ["sobrenome", "Roccella"],
+    ["idade", 40],
+    ["cargo", "Instrutor"],  
+])
+
+
+console.log(entidade.get("sobrenome"));
+console.log(pessoa.sobrenome);
+
+const professor = JSON.stringify(pessoa);
+console.log(professor);
+
+const aluno = '{"nome": "Bento", "sobrenome": "Andrade", "idade": 69, "cargo": "matricula"}';
+const alunoObjeto = JSON.parse(aluno);
+console.log(alunoObjeto);
+console.log(alunoObjeto.nome);
+console.log(aluno.nome);
+
+for (let x in pessoa){
+    console.log(pessoa[x]);     
+}
